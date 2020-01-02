8404cd7f46ef9682db3538a9037551f553953e5f
Saurabh Charde <saurabhchardereal@gmail.com>
2020-01-02 18:11:00 +0530

    X00T: prop: Enable back bluetooth hal
    This reverts commit 37e72702f32769d6ac96dd19d541aae449a654b9.
    
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -86,7 +86,6 @@ vendor.audio.feature.snd_mon.enable=false
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.bluetooth.a2dp_offload.disabled=true \
-    persist.bluetooth.bluetooth_audio_hal.disabled=true \
     vendor.bluetooth.soc=cherokee \
     vendor.qcom.bluetooth.soc=cherokee \
     persist.vendor.bt.aac_frm_ctl.enabled=true \
